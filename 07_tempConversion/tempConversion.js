e01dc0b43db0925e626ba120fe0f04d447508e1e
saradir <[superpostioned+github@gmail.com]>
2024-07-31 20:46:28 +0300

    finish exercises 6+7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(int) {
 
-const convertToFahrenheit = function() {
+  const convertedVal = ((int -32) * (5/9));
+  return  (Math.round(convertedVal * 10) / 10);
+}
+
+const convertToFahrenheit = function(int) {
+  const convertedVal = (int * 9/5) + 32;
+  return  (Math.round(convertedVal * 10) / 10);
 };
 
 // Do not edit below this line
