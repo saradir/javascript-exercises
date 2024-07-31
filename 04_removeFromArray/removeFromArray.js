d0f3a090638cc44f9e91d4626fd4c7221cd4c68c
saradir <[superpostioned+github@gmail.com]>
2024-07-31 18:13:35 +0300

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remValues) {
+
+    const newArray = array.filter(arg => !(remValues.includes(arg)));
+    console.log(newArray);
+    console.log(remValues);
+    return newArray;
 };
 
 // Do not edit below this line
