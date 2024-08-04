cf575f0f2e91c4eddd1da66872b7e1400f2edea5
saradir <[superpostioned+github@gmail.com]>
2024-08-05 01:04:30 +0300

    finish exercise 8 and start some work on 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current);
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for(let i = 1; i <= num; i ++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let arr = str.split("");
+    arr = arr.filter((char) => {
+        if((char >= 'a' && char <='z') || (char >= '0' && char <= '9')){
+            return true;
+        }
+        return false;
+    });
+    arr = arr.reverse().join("");
+    return arr === str;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
